8f4425a094fdce75cf9bcf3a0d14a6beb20e0389
Viktor geozukunft <geozukunft@gmail.com>
2020-05-07 23:07:33 +0200

    10/10 REGEX IMPLEMENTATION

--- CSV-Einlesen/Form1.Designer.cs
+++ CSV-Einlesen/Form1.Designer.cs
@@ -28,13 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dateiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFile_toolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speichernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.speichernSchließenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.löschenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.schließenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lboRawData = new System.Windows.Forms.ListBox();
+            this.lboSum = new System.Windows.Forms.ListBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblInfotext = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dateiToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(3, 1, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(780, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // dateiToolStripMenuItem
+            // 
+            this.dateiToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFile_toolStripMenuItem,
+            this.speichernToolStripMenuItem,
+            this.speichernSchließenToolStripMenuItem,
+            this.löschenToolStripMenuItem,
+            this.schließenToolStripMenuItem});
+            this.dateiToolStripMenuItem.Name = "dateiToolStripMenuItem";
+            this.dateiToolStripMenuItem.Size = new System.Drawing.Size(46, 22);
+            this.dateiToolStripMenuItem.Text = "Datei";
+            // 
+            // OpenFile_toolStripMenuItem
+            // 
+            this.OpenFile_toolStripMenuItem.Name = "OpenFile_toolStripMenuItem";
+            this.OpenFile_toolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.OpenFile_toolStripMenuItem.Text = "Datei öffnen";
+            this.OpenFile_toolStripMenuItem.Click += new System.EventHandler(this.OpenFile_toolStripMenuItem_Click);
+            // 
+            // speichernToolStripMenuItem
+            // 
+            this.speichernToolStripMenuItem.Name = "speichernToolStripMenuItem";
+            this.speichernToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.speichernToolStripMenuItem.Text = "Speichern";
+            this.speichernToolStripMenuItem.Click += new System.EventHandler(this.speichernToolStripMenuItem_Click);
+            // 
+            // speichernSchließenToolStripMenuItem
+            // 
+            this.speichernSchließenToolStripMenuItem.Name = "speichernSchließenToolStripMenuItem";
+            this.speichernSchließenToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.speichernSchließenToolStripMenuItem.Text = "Speichern und Schließen";
+            this.speichernSchließenToolStripMenuItem.Click += new System.EventHandler(this.speichernSchließenToolStripMenuItem_Click);
+            // 
+            // löschenToolStripMenuItem
+            // 
+            this.löschenToolStripMenuItem.Name = "löschenToolStripMenuItem";
+            this.löschenToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.löschenToolStripMenuItem.Text = "Löschen";
+            this.löschenToolStripMenuItem.Click += new System.EventHandler(this.löschenToolStripMenuItem_Click);
+            // 
+            // schließenToolStripMenuItem
+            // 
+            this.schließenToolStripMenuItem.Name = "schließenToolStripMenuItem";
+            this.schließenToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.schließenToolStripMenuItem.Text = "Schließen";
+            this.schließenToolStripMenuItem.Click += new System.EventHandler(this.schließenToolStripMenuItem_Click);
+            // 
+            // lboRawData
+            // 
+            this.lboRawData.FormattingEnabled = true;
+            this.lboRawData.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lboRawData.Location = new System.Drawing.Point(39, 39);
+            this.lboRawData.Name = "lboRawData";
+            this.lboRawData.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.lboRawData.Size = new System.Drawing.Size(135, 199);
+            this.lboRawData.TabIndex = 12;
+            // 
+            // lboSum
+            // 
+            this.lboSum.FormattingEnabled = true;
+            this.lboSum.Location = new System.Drawing.Point(230, 39);
+            this.lboSum.Name = "lboSum";
+            this.lboSum.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.lboSum.Size = new System.Drawing.Size(129, 199);
+            this.lboSum.TabIndex = 13;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(39, 281);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(177, 20);
+            this.txtInput.TabIndex = 14;
+            this.txtInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput_KeyPress);
+            // 
+            // lblInfotext
+            // 
+            this.lblInfotext.AutoSize = true;
+            this.lblInfotext.Location = new System.Drawing.Point(36, 304);
+            this.lblInfotext.Name = "lblInfotext";
+            this.lblInfotext.Size = new System.Drawing.Size(294, 13);
+            this.lblInfotext.TabIndex = 15;
+            this.lblInfotext.Text = "Daten bitte mit \";\" getrennt einfügen und mit Enter bestätigen";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(780, 566);
+            this.Controls.Add(this.lblInfotext);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lboSum);
+            this.Controls.Add(this.lboRawData);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dateiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenFile_toolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speichernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem speichernSchließenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem schließenToolStripMenuItem;
+        private System.Windows.Forms.ListBox lboRawData;
+        private System.Windows.Forms.ListBox lboSum;
+        private System.Windows.Forms.TextBox txtInput;
+        public System.Windows.Forms.Label lblInfotext;
+        private System.Windows.Forms.ToolStripMenuItem löschenToolStripMenuItem;
     }
 }
 
--- CSV-Einlesen/Form1.cs
+++ CSV-Einlesen/Form1.cs
@@ -7,14 +7,223 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
 
 namespace CSV_Einlesen
 {
     public partial class Form1 : Form
     {
+        string datafile; //Path to file
+        string filename; //filename
+        ArrayList dataStream = new ArrayList(); //Array that hold raw data
+        ArrayList sumList = new ArrayList(); //Array for Sums of Lines
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenFile_toolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofdData = new OpenFileDialog()
+            {
+                InitialDirectory = @"C:\",
+
+                Title = "Datei öffnen",
+
+                DefaultExt = "csv",
+                Filter = "CSV Datei (*.csv)|*.csv|Alle Dateien (*.*)|*.*",
+                FilterIndex = 0,
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                Multiselect = false
+            };
+
+            if (ofdData.ShowDialog() == DialogResult.OK)
+            {
+                datafile = ofdData.FileName;
+                filename = Path.GetFileName(datafile);
+
+                readData();
+                updateData();
+
+            }
+
+
+
+        }
+
+        private void schließenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Sind Sie sicher, dass sie das Programm beenden möchten ohne vorher zu speichern?", "Schließen ohne Speichern", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+
+                Application.Exit();
+            }
+        }
+
+        private void speichern()
+        {
+            if (datafile == null)
+            {
+                MessageBox.Show("Keine Datei zum speichern ausgwählt bitte öffnen sie zuerst eine Datei!");
+            }
+            else
+            {
+                File.WriteAllText(datafile, String.Empty);
+                using (StreamWriter file = File.AppendText(datafile))
+                {
+                    foreach (string line in dataStream)
+                        file.WriteLine(line);
+
+                    file.Close();
+                }
+            }
+        }
+
+        private void speichernSchließenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (datafile == null)
+            {
+                MessageBox.Show("Keine Datei zum speichern ausgwählt bitte öffnen sie zuerst eine Datei!");
+            }
+            else
+            {
+                File.WriteAllText(datafile, String.Empty);
+                using (StreamWriter file = File.AppendText(datafile))
+                {
+
+                    foreach (string line in dataStream)
+                        file.WriteLine(line);
+
+                    file.Close();
+                }
+
+                Application.Exit();
+            }
+        }
+
+        private void speichernToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            speichern();
+        }
+
+        private void löschenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(datafile == null)
+            {
+                MessageBox.Show("Keine Datei geöffnet welche gelöscht werden kann.");
+            }
+            else
+            {
+                DialogResult result = MessageBox.Show(String.Format("Sind Sie sicher, dasss sie die Datei {0} löschen möchten?",filename), "Datei Löschen" ,MessageBoxButtons.OKCancel);
+                if(result == DialogResult.OK)
+                {
+                    File.Delete(datafile);
+                }
+            }
+        }
+
+        private void readData()
+        {
+            foreach(string line in File.ReadLines(datafile))
+                {
+                dataStream.Add(line); 
+            }
+        }
+
+        private void updateData()
+        {
+            lboRawData.Items.Clear();
+            lboSum.Items.Clear();
+            sumList.Clear();
+
+            foreach (string line in dataStream)
+                lboRawData.Items.Add(line);
+
+            foreach(string line in dataStream)
+            {
+                int sum = 0;
+                string[] splitLine = line.Split(';');
+
+                foreach(string splitPart in splitLine)
+                {
+                    sum += Convert.ToInt32(splitPart);
+                }
+
+                sumList.Add(sum);
+            }
+
+            foreach(int sum in sumList)
+            {
+                lboSum.Items.Add(sum);
+            }
+
+        }
+
+        private void txtInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                if(budgetRegex(txtInput.Text))
+                {
+                    dataStream.Add(txtInput.Text);
+                    updateData();
+                    txtInput.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Bitte überprüfen Sie Ihre Eingabe. Format: ZAHL;ZAHL;ZAHL", "Fehlerhafte Eingabe", MessageBoxButtons.OK);
+                    txtInput.Clear();
+                }
+            }
+        }
+
+        private bool budgetRegex(string InputData)  //Manually Coded a Regex to check for wrong inputs
+        {
+            char[] splitData = InputData.ToCharArray();
+            int DataLength = splitData.Length;
+            bool doubleCheck = false;
+
+            if (DataLength == 0)
+                return false;
+
+            if (splitData[0] == ';' || splitData[DataLength - 1] == ';')
+                return false;
+
+            foreach (char splitPart in splitData)
+            {
+              
+                if (!(splitPart.Equals('1') || splitPart.Equals('2') || splitPart.Equals('3') || splitPart.Equals('4') || splitPart.Equals('5') || splitPart.Equals('6') || splitPart.Equals('7') || splitPart.Equals('8') || splitPart.Equals('9') || splitPart.Equals('0') || splitPart.Equals(';')))
+                {
+                    return false;
+                }
+
+                if(splitPart == ';' && doubleCheck == true)
+                {
+                    return false;
+                }
+                else if(splitPart == ';')
+                {
+                    doubleCheck = true;
+                }
+                else
+                {
+                    doubleCheck = false;
+                }
+            }
+
+            return true;
         }
     }
 }

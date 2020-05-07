8fa86d83e37c0f9bfe67b54a7208bf20ef56b1d7
Viktor geozukunft <geozukunft@gmail.com>
2020-05-07 23:41:07 +0200

    Publish

--- CSV-Einlesen/Form1.Designer.cs
+++ CSV-Einlesen/Form1.Designer.cs
@@ -148,7 +148,7 @@
             this.Controls.Add(this.lboRawData);
             this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "CSV - Daten";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
